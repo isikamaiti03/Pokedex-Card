8f5b62d6625fd56d8bbff874f8b596b7dd9092f3
Isika Maiti <it2027@cemk.ac.in>
2024-09-02 13:40:01 +0530

    Update Pokedex.js

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,15 +1,11 @@
 import React,{useState} from 'react'
-//import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-//import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './Pokedex.css';
 const Pokedex = (props) => {
   const navigate=useNavigate();
   const [activeCard, setActiveCard] = useState(null);
   const handleImageClick=(index)=>{
-    // const card=document.querySelector('.CardMain')
-    // card.classList.add('with-border');
     setActiveCard(props.index);
     setTimeout(()=>{ 
       navigate('/PokedexDetails',{state:{...props}})
@@ -18,12 +14,6 @@ const Pokedex = (props) => {
   return (
    <>
       <Card className={`CardMain ${activeCard === props.index ? 'with-border' : ''}`}>
-        {/* <Link to="/PokedexDetails">
-        <div className='Imagediv' onClick={handleImageClick}>
-        <Card.Img variant="top" style={{height:"150px",width:"170px",paddingLeft:"30px"}}
-         src={props.img}/>
-        </div>
-        </Link> */}
         <div className='Imagediv' onClick={()=>handleImageClick(props.index)}>
         <Card.Img variant="top" style={{height:"150px",width:"170px",paddingLeft:"30px"}}
          src={props.img}/>
