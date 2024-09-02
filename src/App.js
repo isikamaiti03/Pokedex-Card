4d6065ebfc5670d114ede65cf689da2ae281890b
Isika Maiti <it2027@cemk.ac.in>
2024-09-02 13:30:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-// import Reg from "./Reg";
-// import React, { Component } from "react";
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
+import React Component  from "react";
 import { Routes, Route } from "react-router-dom";
 import PokedexMain from "./PokedexMain";
 import PokedexDetails from "./PokedexDetails";
 
-//import './App.css';
-
 const App = () => {
  
   return (
-    <>
-    
-    <Routes>
-      <Route path="/" element={<PokedexMain/>} />
-      <Route path="/PokedexDetails" element={<PokedexDetails/>}/>
-    </Routes>
-      
+    <>    
+      <Routes>
+          <Route path="/" element={<PokedexMain/>} />
+          <Route path="/PokedexDetails" element={<PokedexDetails/>}/>
+      </Routes>      
    </> 
   );
 };
